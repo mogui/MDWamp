f85579f859150fd661f1a1ea29fbc99b33511336
Niko Usai <niko.usai@it.gucci.com>
2013-12-13 17:39:35 +0100

    Restructured Project, empty source, two targets and tests SocketRocket as Submodule / Subproject

---
+++ MDWamp/MDWamp.h
@@ -0,0 +1,13 @@
+//
+//  MDWamp.h
+//  MDWamp
+//
+//  Created by Niko Usai on 13/12/13.
+//  Copyright (c) 2013 mogui.it. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDWamp : NSObject
+- (void)test;
+@end
---
+++ MDWamp/MDWamp.m
@@ -0,0 +1,16 @@
+//
+//  MDWamp.m
+//  MDWamp
+//
+//  Created by Niko Usai on 13/12/13.
+//  Copyright (c) 2013 mogui.it. All rights reserved.
+//
+
+#import "MDWamp.h"
+
+@implementation MDWamp
+- (void)test
+{
+    NSLog(@"-----");
+}
+@end
