389634db32871beee726a717a7864a6b4180e768
Niko Usai <niko.usai@it.gucci.com>
2014-04-28 22:38:54 +0200

    all Messages and tests done

--- MDWamp/MDWampMessages/MDWampInvocation.m
+++ MDWamp/MDWampMessages/MDWampInvocation.m
@@ -10,4 +10,32 @@
 
 @implementation MDWampInvocation
 
+
+- (id)initWithPayload:(NSArray *)payload
+{
+    self = [super init];
+    if (self) {
+        NSMutableArray *tmp = [payload mutableCopy];
+        // [INVOCATION, Request|id, REGISTERED.Registration|id, Details|dict, CALL.Arguments|list, CALL.ArgumentsKw|dict]
+        
+        self.request   = [tmp shift];
+        self.registration    = [tmp shift];
+        self.details        = [tmp shift];
+        if ([tmp count] > 0) self.arguments     = [tmp shift];
+        if ([tmp count] > 0) self.argumentsKw   = [tmp shift];
+    }
+    return self;
+}
+
+- (NSArray *)marshallFor:(MDWampVersion)version
+{
+    if (self.arguments && self.argumentsKw) {
+        return @[@68, self.request, self.registration, self.details, self.arguments, self.argumentsKw ];
+    } else if(self.arguments) {
+        return @[@68, self.request, self.registration, self.details, self.arguments ];
+    } else {
+        return @[@68, self.request, self.registration, self.details];
+    }
+}
+
 @end
